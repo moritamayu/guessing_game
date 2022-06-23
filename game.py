7bfcf4b7b2438f5dd5a4c0811326702d6df5bc48
moritamayu <mayu_1537@asagi.waseda.jp>
2022-06-23 13:51:37 +0900

    add hint function and change main program to repetition

--- game.py
+++ game.py
@@ -1,21 +1,27 @@
 import numpy as np
 import sys
 
-def judgment(i, j):
-    if i==j:
+def judgment(j, k):
+    if j==k:
         print("正解です！")
         sys.exit()
     else:
         print("不正解です．")
 
-def hint1(i, j):
-    if i>j:
+def hint(i, j, k):
+    if i==1:
+        hint1(j, k)
+    elif i==2:
+        hint2(k)
+
+def hint1(j, k):
+    if j>k:
         print("もっと小さい数字です．")
     else:
         print("もっと大きい数字です．")
 
-def hint2(i, j):
-    if j%2==0:
+def hint2(k):
+    if k%2==0:
         print("正解の数字は偶数です．")
     else:
         print("正解の数字は奇数です．")
@@ -23,10 +29,11 @@ def hint2(i, j):
 num = np.random.randint(100, 1000, (1, 1))
 print(num)
 
-print("三桁の数字を入力してください．")
-num_player = int (input())
+for i in range(1, 3):
+    print("三桁の数字を入力してください．")
+    num_player = int (input())
 
-judgment(num_player, num)
+    judgment(num_player, num)
 
-print("ヒント２：", end="")
-hint2(num_player, num)
+    print("ヒント%d : " %i, end="")
+    hint(i, num_player, num)
