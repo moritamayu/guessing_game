09393646364fc6bdda71ed6d32081d5ebf57f44c
moritamayu <mayu_1537@asagi.waseda.jp>
2022-06-23 11:51:31 +0900

    add judgment function

--- game.py
+++ game.py
@@ -1,18 +1,23 @@
 import numpy as np
+import sys
+
+def judgment(i, j):
+    if i==j:
+        print("正解です！")
+        sys.exit()
+    else:
+        print("不正解です．")
 
 num = np.random.randint(100, 1000, (1, 1))
-##print(num)
+print(num)
 
 print("三桁の数字を入力してください．")
 num_player = int (input())
 
-if num_player==num:
-    print("正解です！")
+judgment(num_player, num)
 
+print("ヒント１：", end="")
+if num_player>num:
+    print("もっと小さい数字です．")
 else:
-    print("不正解です．")
-    print("ヒント１：", end="")
-    if num_player>num:
-        print("もっと小さい数字です．")
-    else:
-        print("もっと大きい数字です")
+    print("もっと大きい数字です")
